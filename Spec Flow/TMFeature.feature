2016264fbc6ebad91e98340ada88c0683644b450
sree-test254 <sreepriyakrishnan1993@gmail.com>
2025-03-14 11:18:13 +1300

    SPECFLOW and StepDefinition implemented"

---
+++ Spec Flow/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a TurnUp portal admin user
+I would like to create,edit and delete time and materials records
+so that I can manage employees time and materials successfully.
+
+@regression 
+Scenario: create time record with valid data
+	Given I logged into TurnUp portal successfully
+	When I Navigate to Time and Materials page
+	When I create a time record
+	Then record created successfully
---
+++ Step Definition/TMFeatureStepDefinitions.cs
@@ -0,0 +1,35 @@
+using System;
+using SpecFlow;
+using TechTalk.SpecFlow;
+
+namespace Feb25TurnUpPortal1.StepDefinition
+
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given("I logged into TurnUp portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I Navigate to Time and Materials page")]
+        public void WhenINavigateToTimeAndMaterialsPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I create a time record")]
+        public void WhenICreateATimeRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("record created successfully")]
+        public void ThenRecordCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
